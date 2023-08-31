b89781cba108a427f284a614376753f8c13af406
Mahmudul Hasan <mahmudulmr19@gmail.com>
2023-08-31 19:41:28 +0600

    updated env and added trpc client setup

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,7 +1,7 @@
+// trpc.ts
 import { initTRPC, TRPCError } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
-import superjson from "superjson";
 import { ZodError } from "zod";
 import { getServerAuthSession } from "@/server/auth";
 import { prisma } from "./db";
@@ -26,7 +26,6 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
 };
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
-  transformer: superjson,
   errorFormatter({ shape, error }) {
     return {
       ...shape,
